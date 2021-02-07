251ac323e9c328230c07917d4fc486b6029b42f5
Marcos Vinícius <marcosvinist98@gmail.com>
2021-02-07 14:09:13 -0300

    first commit

---
+++ precache-manifest.6be610fce170b15e0c7893ee8e6ede04.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59ed091d3e914b3eb8fb",
+    "url": "/ondetem-vue/css/app.1fa8772a.css"
+  },
+  {
+    "revision": "9dba81487b0bad70760f7116765f1ae0",
+    "url": "/ondetem-vue/img/fox.9dba8148.jpg"
+  },
+  {
+    "revision": "a530ff74f858083178e4663ef57fe6f1",
+    "url": "/ondetem-vue/index.html"
+  },
+  {
+    "revision": "18d9a7d30f01bdd5bb83",
+    "url": "/ondetem-vue/js/about.eab6dda0.js"
+  },
+  {
+    "revision": "59ed091d3e914b3eb8fb",
+    "url": "/ondetem-vue/js/app.74e328f6.js"
+  },
+  {
+    "revision": "05e93f74f28bac219399",
+    "url": "/ondetem-vue/js/chunk-vendors.43085f26.js"
+  },
+  {
+    "revision": "1b04d0c9a258f7a9e0503df1f2372971",
+    "url": "/ondetem-vue/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/ondetem-vue/robots.txt"
+  }
+]);
